eb86a6899e10bdcd9910746bfe86b13bdd3418a6
Rounak-kedia <43108913+Rounak-kedia@users.noreply.github.com>
2019-09-24 15:15:50 +0530

    Add files via upload

---
+++ Prog4.py
@@ -0,0 +1,36 @@
+class Student:
+	def __init__(self):
+		self.id = None
+		self.age = None
+		self.marks = None
+
+	def val_marks(self):
+		return self.marks >= 0 and self.marks <=100
+
+	def val_age(self):
+		return self.age>20
+
+	def check_qual(self):
+		if(self.val_marks() and self.val_age()):
+			return self.marks>=65
+		print("\nData NOT Valid.")s
+		return False
+	    
+	def set(self):
+		self.id = input("ID : ")
+		self.age = int(input("AGE : "))
+		self.marks = float(input("MARKS : "))
+
+	def get(self):
+		print("ID : ",self.id)
+		print("AGE : ",self.age)
+		print("MARKS : ",self.marks)
+
+S = Student()
+S.set()
+S.get()
+if S.check_qual():
+    print("\nThe Student is qualified for admission.")
+else:
+    print("\nThe Student is NOT qualified for admission.")
+    
